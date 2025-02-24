207dc1dcc30e368f06703cc33d2c1233254bcfa0
Pedro Mocco <pedromocco00@gmail.com>
2025-02-24 08:43:20 -0400

    Directory changes

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -2,12 +2,22 @@ import { bot, chatIds } from "../../../utils/bot";
 import { generatePDF } from "../../../utils/pdfGenerator";
 import { ensureDirectoryExists } from "../../../utils/utils";
 import path from "path";
+import os from "os";
 const fs = require("fs");
+
+function getRecordsPath() {
+  if (process.env.NODE_ENV === "production") {
+    return path.join(os.tmpdir(), "userData", "records");
+  } else {
+    return path.join(process.cwd(), "userData", "records");
+  }
+}
 
 export default async function handler(req, res) {
   try {
     const data = req.body;
-    const recordsPath = path.join(process.cwd(), "userData", "records");
+
+    const recordsPath = getRecordsPath();
     ensureDirectoryExists(recordsPath);
 
     const { filePath, filename } = await generatePDF(data, recordsPath);
@@ -20,6 +30,7 @@ export default async function handler(req, res) {
     }
 
     fs.unlinkSync(filePath);
+
     res.status(200).json({ success: true, message: "Archivo PDF enviado con éxito" });
   } catch (error) {
     console.error("Error:", error);
